05ac2248871d3015781047a954155a50f3f340ad
Isaac Essuman <isaacessuman@gmail.com>
2025-06-25 21:39:33 -0400

    Initialize project structure with placeholders

---
+++ contracts/Lottery.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// TODO: implement the lottery smart contract logic
+contract Lottery {
+    // TODO: state variables
+
+    constructor() {
+        // TODO: initialization code
+    }
+
+    // TODO: functions to enter, pick winner, etc.
+}
